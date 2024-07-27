54fd53df526c4bea293ab2a806e410fd01a1aeb3
Charles Christolini <code@binarypie.com>
2024-07-27 00:51:18 -0700

    pass db directly into Fuego

--- src/classes/Fuego.ts
+++ src/classes/Fuego.ts
@@ -1,10 +1,7 @@
-import { FirebaseOptions, getApp, getApps, initializeApp } from '@firebase/app'
-import { getFirestore, Firestore } from '@firebase/firestore'
+import { Firestore } from '@firebase/firestore'
 export class Fuego {
   public db: Firestore
-  constructor(config: FirebaseOptions) {
-    this.db = !getApps().length
-      ? getFirestore(initializeApp(config))
-      : getFirestore(getApp())
+  constructor(db: Firestore) {
+    this.db = db;
   }
 }
